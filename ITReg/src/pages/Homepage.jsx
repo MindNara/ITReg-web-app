f6e0ee869dbb4479163092cbbbd8a5321d862eb2
MindNara <88419813+MindNara@users.noreply.github.com>
2024-01-05 16:27:29 +0700

    Initial commit

---
+++ ITReg/src/pages/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Homepage() {
+
+    return (
+        <>
+            <h1 className="text-3xl font-bold">Homepage</h1>
+        </>
+    );
+}
+
+export default Homepage;
